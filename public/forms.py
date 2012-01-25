889ed545105a5dd67c85395cd2f392d33e8c97e6
Valder Gallo <valdergallo@gmail.com>
2012-01-25 11:06:16 -0200

    Fix searchform

--- public/forms.py
+++ public/forms.py
@@ -1,15 +1,17 @@
 # encoding: utf-8
+import re
+
 from django import forms
 from django.db.models import Q
 from django.template.defaultfilters import slugify
+
 from public.models import Public
-import re
 
 
 class SearchForm(forms.Form):
     "Search values on public content everywere"
 
-    search = forms.CharField(max_length=100)
+    search = forms.CharField(max_length=100, required=False)
 
     def get_result_queryset(self):
         query = Q()
@@ -28,7 +30,7 @@ class SearchForm(forms.Form):
 class TagForm(forms.Form):
     "Add tags and split multiple values from one string like ,/ ... "
 
-    tags = forms.CharField(max_length=255, null=True, blank=True)
+    tags = forms.CharField(max_length=255, required=False)
 
     def clean_tags(self):
         value = self.cleaned_data['tags']
