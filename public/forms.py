2269efe6d16df379a31548054b42ee8f313832df
Valder Gallo <valdergallo@gmail.com>
2012-01-25 10:41:18 -0200

    Add slug fields and help text

--- public/forms.py
+++ public/forms.py
@@ -1,9 +1,14 @@
 # encoding: utf-8
 from django import forms
 from django.db.models import Q
+from django.template.defaultfilters import slugify
 from public.models import Public
+import re
+
 
 class SearchForm(forms.Form):
+    "Search values on public content everywere"
+
     search = forms.CharField(max_length=100)
 
     def get_result_queryset(self):
@@ -18,3 +23,26 @@ class SearchForm(forms.Form):
         query |= Q(publictag__tag__icontains=value)
 
         return value, Public.objects.filter(query)
+
+
+class TagForm(forms.Form):
+    "Add tags and split multiple values from one string like ,/ ... "
+
+    tags = forms.CharField(max_length=255, null=True, blank=True)
+
+    def clean_tags(self):
+        value = self.cleaned_data['tags']
+        strip_tags = re.split('; | , - /', value)
+        if len(strip_tags):
+            return [slugify(tag) for tag in strip_tags]
+        else:
+            return slugify(strip_tags)
+
+
+class PublicForm(forms.ModelForm):
+    "Add new Public"
+
+    class Meta:
+        model = Public
+        fields = ('title', 'tie', 'message', 'image')
+
