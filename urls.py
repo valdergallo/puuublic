64c41257489adad27b30ed2cfdb3ac6596c06c16
Valder Gallo <valdergallo@gmail.com>
2012-01-29 20:55:10 -0200

    [completed: 64] termos de acesso

--- urls.py
+++ urls.py
@@ -4,8 +4,9 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    #url(r'^accounts/', include('registration.urls')),
     url(r'^', include('website.urls' , app_name='website', namespace='website')),
+    url(r'^accounts/login/', 'website.views.home'),
+    url(r'^accounts/', include('registration.urls')),
     url(r'^public/', include('public.urls' , app_name='public', namespace='public')),
     url(r'^pubblic_admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^pubblic_admin/', include(admin.site.urls)),
--- website/urls.py
+++ website/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls.defaults import patterns, url
+from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('website.views',
     url(r'^$', 'home', name='home'),
-    url(r'^(?P<username>\w+)/$', 'home_user', name='home_user'),
+    url(r'^u/(?P<username>\w+)/$', 'home_user', name='home_user'),
     url(r'^ajax_login/$', 'ajax_login', name='ajax_login'),
     url(r'^logout/$', 'weblogout', name='weblogout'),
     url(r'^novidades/$', 'novidades', name='novidades'),
     url(r'^institucional/$', 'institucional', name='institucional'),
-    url(r'^termos/$', 'termos', name='termos'),
+    url(r'^termos/$', direct_to_template , {'template': 'website/termos.html'} , name='termos'),
     url(r'^contato/$', 'contato', name='contato'),
 )
