0bc38583a0fc0065fd17c58e215e5f57c541e0f6
Valder Gallo <valdergallo@gmail.com>
2012-01-29 23:40:57 -0200

    missing files website

---
+++ website/admin.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+admin.py
+
+Created by Valder Gallo on 2012-01-29.
+Copyright (c) 2012 valdergallo. All rights reserved.
+"""
+
+from django.contrib import admin
+from models import Blog, Category
+from sorl.thumbnail.admin import AdminImageMixin
+
+
+class BlogAdmin(admin.ModelAdmin):
+    pass
+    
+    
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+    
+admin.site.register(Blog, BlogAdmin)
+admin.site.register(Category, CategoryAdmin)
