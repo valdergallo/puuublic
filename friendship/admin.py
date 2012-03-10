9c66e8d7677fd589a1b3120a60a5d1ee25f84249
Valder Gallo <valdergallo@gmail.com>
2012-03-09 23:53:32 -0300

    add user on admin

---
+++ friendship/admin.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+admin.py
+
+Created by Valder Gallo on 2012-01-29.
+Copyright (c) 2012 valdergallo. All rights reserved.
+"""
+
+from django.contrib import admin
+from models import UserProfile, Group, Private, FriendshipType, Friendship, Follow
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
+
+
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    max_num = 1
+    can_delete = False
+
+
+class UserAdmin(AuthUserAdmin):
+    inlines = [UserProfileInline]
+
+# unregister old user admin
+admin.site.unregister(User)
+# register new user admin
+admin.site.register(User, UserAdmin)
+
+admin.site.register(Group)
+admin.site.register(Private)
+admin.site.register(FriendshipType)
+admin.site.register(Friendship)
+admin.site.register(Follow)
