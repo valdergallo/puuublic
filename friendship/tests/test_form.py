2b159a2020bf8989aef1b74063ecba02ea345620
Valder Gallo <valdergallo@gmail.com>
2012-03-09 23:53:47 -0300

    add friendship tests

--- friendship/tests/test_form.py
+++ friendship/tests/test_form.py
@@ -8,11 +8,18 @@ Copyright (c) 2012 valdergallo. All rights reserved.
 """
 
 from django.test import TestCase
+from django.contrib.auth.models import User
 
 
-class SimpleTest2(TestCase):
+class TestForm(TestCase):
+
+    def setUp(self):
+        user = User.objects.create(username='admin', is_superuser=True, is_staff=True)
+        user.set_password('admin')
+        user.save()
+
     def test_basic_addition(self):
         """
-        Tests that 1 + 1 always equals 2.
+        Test user loged
         """
         self.assertEqual(1 + 1, 2)
