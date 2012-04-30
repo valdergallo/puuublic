3fcfc2b9c81e77dac8c3ca1307dac1e6e5b463cf
Valder Gallo <valdergallo@gmail.com>
2012-04-30 09:31:45 -0300

    Remove path to db for dreamhost

--- puuublic/settings.py
+++ puuublic/settings.py
@@ -14,7 +14,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', 
         # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.realpath(os.path.join(BASEDIR, 'db', 'puuublic.sqlite')), 
+        'NAME': 'puuublic.sqlite', 
         # Or path to database file if using sqlite3.
         'USER': '', # Not used with sqlite3.
         'PASSWORD': '', # Not used with sqlite3.
