4de304bdc4720aafb985843756b396249abd06e3
Valder Gallo <valdergallo@gmail.com>
2012-05-01 12:33:01 -0300

    timezone UTC = false

--- puuublic/settings.py
+++ puuublic/settings.py
@@ -32,7 +32,7 @@ LANGUAGE_CODE = 'pt-BR'
 SITE_ID = 1
 USE_I18N = True
 USE_L10N = True
-USE_TZ = True
+USE_TZ = False
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
